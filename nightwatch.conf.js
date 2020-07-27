896642759cb4fddc7b72eb6092b9179de9462d2a
Jonathan M <jonathan.milliere@gmail.com>
2020-07-27 23:12:31 +0200

    'init'

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -26,7 +26,7 @@ module.exports = {
         javascriptEnabled: true,
         acceptSslCerts: true,
         chromeOptions: {
-          //args: ["--headless"]
+          args: ["--headless"]
        }
       },
       screenshots: {
